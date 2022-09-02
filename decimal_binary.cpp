67b3948968f01ed182d1a1daf53508e70b0e34bf
Aman Mahesh Chavan <111043080+aman7867chavan@users.noreply.github.com>
2022-09-02 16:10:19 +0530

    Decimal to binary

---
+++ decimal_binary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n, i, a[10];
+    cout << "Enter number\n";
+    cin >> n;
+    for (i = 0; n > 0; i++)
+    {
+
+        a[i] = n % 2;
+        n = n / 2;
+    }
+
+    for (i = i - 1; i >= 0; i--)
+    {
+        cout << a[i];
+    }
+}
